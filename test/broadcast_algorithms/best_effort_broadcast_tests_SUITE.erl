d8a50e7d978ca94aeb12e6768654a610b0933235
Julian Haas <jhaas@mpi-sws.org>
2021-07-30 15:59:17 +0200

    delete old testcases (aka "Altlasten")

--- test/broadcast_algorithms/best_effort_broadcast_tests_SUITE.erl
+++ test/broadcast_algorithms/best_effort_broadcast_tests_SUITE.erl
@@ -38,23 +38,6 @@ end_per_testcase(_, Config) ->
   Config.
 
 % a simple chat server for testing the broadcast:
-chat_loop(MIL, LL, Name, Received) ->
-    {ok, B} = best_effort_broadcast:start_link(MIL, LL, self()),
-    NewName = list_to_atom(atom_to_list(Name) ++ pid_to_list(B)),
-    gen_server:cast(MIL, {register, {NewName, B, best_effort_broadcast}}),
-    receive
-        {post, From, Msg} ->
-            best_effort_broadcast:broadcast(B, Msg),
-            From ! {self(), ok},
-            chat_loop(MIL, LL, Name, Received);
-        {deliver, Msg} ->
-            chat_loop(MIL, LL, Name, [Msg|Received]);
-        {get_received, From} ->
-            From ! {self(), Received},
-            chat_loop(MIL, LL, Name, Received)
-    end.
-
-% a simple chat server for testing the broadcast:
 chat_loop_simplified(LL, Name, Received) ->
     % initialize broadcast process
     {ok, B} = best_effort_broadcast_paper:start_link(LL, Name, self()),
@@ -137,32 +120,6 @@ no_crash_test_simple(_Config) ->
     basic_tests_SUITE:assert_equal(['Hello everyone!'], Received1),
     basic_tests_SUITE:assert_equal(['Hello everyone!'], Received2),
     basic_tests_SUITE:assert_equal(['Hello everyone!'], Received3).
-
-with_crash_test() ->
-    % Create dummy link layer for testing:
-    TestNodes = [nodeA, nodeB, nodeC],
-    {ok, LLD, [LL1, LL2, LL3]} = link_layer_dummy:start_link(TestNodes),
-
-    % Create 3 chat servers:
-    Chat1 = spawn_link(fun() -> chat_loop(undefined, LL1, bc1, []) end),
-    Chat2 = spawn_link(fun() -> chat_loop(undefined, LL2, bc2, []) end),
-    Chat3 = spawn_link(fun() -> chat_loop(undefined, LL3, bc3, []) end),
-
-    % post a message to chatserver 1
-    Chat1 ! {post, self(), 'Hello everyone!'},
-    receive {Chat1, ok} -> ok end,
-
-    % deliver one message from nodeA to nodeB:
-    link_layer_dummy:deliver(LLD, nodeA, nodeB, 0),
-    % then crash nodeA -> message from nodeA to nodeC is lost
-    link_layer_dummy:crash(LLD, nodeA),
-    % finish sending all messages
-    link_layer_dummy:finish(LLD),
-
-    % We can only expect, that Chat2 has received the message:
-    Chat2 ! {get_received, self()},
-    receive {Chat2, Received2} -> ok end,
-    basic_tests_SUITE:assert_equal(['Hello everyone!'], Received2).
 
 with_crash_test_simple(_Config) ->
     % Create link layer for testing:

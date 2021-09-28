68d6392f81b50e4666e21f709d9b34d47db402d1
Julian Haas <jhaas@mpi-sws.org>
2021-09-28 12:52:55 +0200

    fix error in message type decl

--- src/broadcast_algorithms/bc_types.hrl
+++ src/broadcast_algorithms/bc_types.hrl
@@ -2,4 +2,4 @@
 -type bc_message() :: beb_message()
                  | rb_message().
 -type beb_message() :: nonempty_string().
--type rb_message() :: {atom(), non_neg_integer(), beb_message()}.
+-type rb_message() :: {atom() | nonempty_string(), non_neg_integer(), beb_message()}.

95ed6a05aaa2752b08b13e3cb5fe63499916202e
Julian Haas <jhaas@mpi-sws.org>
2021-10-08 15:18:57 +0200

    move validity property

--- src/observables/broadcast/properties/validity.erl
+++ src/observables/broadcast/properties/validity.erl
@@ -1,4 +1,4 @@
--module(broadcast_observer).
+-module(validity).
 -behaviour(gen_event).
 
 -include("observer_events.hrl").
@@ -6,7 +6,6 @@
 -export([init/1, handle_call/2, handle_event/2, teminate/2]).
 
 -record(state, {
-    history_of_events :: queue:queue(),
     broadcast_p = maps:new() :: #{nonempty_string() => sets:set(bc_types:message())}, % broadcast events per process
     delivered_p = maps:new() :: #{nonempty_string() => sets:set(bc_types:message())}, % delivered events per process
     validity_p = maps: new() :: #{nonempty_string() => boolean()} % keeps track of valid processes
@@ -14,7 +13,7 @@
 
 %% property functions
 % validity: every correct process that broadcasts a message, eventually delivers this message
--spec check_validity(#state{history_of_events::queue:queue(_), broadcast_p::#{nonempty_string()=>sets:set(_)}, delivered_p::#{nonempty_string()=>sets:set(_)}, validity_p::#{nonempty_string()=>boolean()}}) -> #state{history_of_events::queue:queue(_), broadcast_p::#{nonempty_string()=>sets:set(_)}, delivered_p::#{nonempty_string()=>sets:set(_)}, validity_p::#{nonempty_string()=>boolean()}}.
+-spec check_validity(#state{}) -> #state{}.
 check_validity(State) ->
     CheckPerProcess = fun(Proc,Broadcast) ->
         % substract delivered messages from broadcast messages to check if there are messages that
@@ -30,7 +29,7 @@ check_properties(State) ->
 
 init(_) ->
     io:format("[bc_observer] Started observer."),
-    {ok, #state{history_of_events = queue:new()}}.
+    {ok, #state{}}.
 
 % handles broadcast messages
 handle_event({process, #obs_process_event{process = Proc, event_type = bc_broadcast_event, event_content = #bc_broadcast_event{message = Msg}}}, State) ->
@@ -50,11 +49,6 @@ handle_event({process, #obs_process_event{process = Proc, event_type = bc_delive
     PropertiesChecked = check_properties(NewState),
     io:format("[broadcast_observer] process ~s delivered message: ~p. Validity: ~p~n", [Proc, Msg, maps:get(Proc, PropertiesChecked#state.validity_p, true)]),
     {ok, PropertiesChecked};
-handle_event({sched, SchedEvent}, State) ->
-%%    store event in history of events
-    NewState = add_to_history(State, {sched, SchedEvent}),
-    % TODO: do sth. concrete for observer here
-    {ok, NewState};
 handle_event(Event, State) ->
     io:format("[broadcast_observer] received unhandled event: ~p~n", [Event]),
     {ok, State}.
@@ -62,11 +56,6 @@ handle_event(Event, State) ->
 handle_call(Msg, State) ->
     io:format("[broadcast_observer] received unhandled call: ~p~n", [Msg]),
     {ok, ok, State}.
-
--spec add_to_history(#state{history_of_events::queue:queue(_)}, {'process', _} | {'sched', _}) -> #state{history_of_events::queue:queue(_)}.
-add_to_history(State, GeneralEvent) ->
-    NewHistory = queue:in(GeneralEvent, State#state.history_of_events),
-    State#state{history_of_events = NewHistory}.
 
 teminate(Reason, _State) ->
     io:format("[broadcast_observer] Terminating. Reason: ~p~n", [Reason]).

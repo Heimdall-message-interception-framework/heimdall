330740ad3627f7bc0efc4f2b139a23fb1b636b14
Felix Stutz <fstutz@mpi-sws.org>
2021-11-24 10:02:29 +0100

    retrieved missing type info

--- include/observer_events.hrl
+++ include/observer_events.hrl
@@ -18,8 +18,19 @@
 %% TODO: solve how to use record as types...
 %%-type obs_sched_event() :: sched_event(). % wrong type, also record here
 
+-type process_identifier() :: pid() | atom() | nonempty_string().
+
 -record(obs_process_event, {
-  process :: pid() | atom(),
-  event_type :: any(),
+  process :: process_identifier(),
+  event_type :: atom(),
   event_content :: any()
+}).
+
+%% TODO: move to its own file (or merge others to this one)
+%% process-local broadcast events
+-record(bc_broadcast_event, { % newly broadcast message
+  message :: any() % the message
+}).
+-record(bc_delivered_event, { % newly delivered message
+  message :: any() % the message
 }).

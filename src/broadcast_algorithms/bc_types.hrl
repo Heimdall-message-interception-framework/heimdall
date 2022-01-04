ea7968ac41f8ce67ed31ce48e736277936ff56c4
Julian Haas <github@julian-haas.de>
2022-01-04 18:06:03 +0100

    move bc algos to own repo

--- src/broadcast_algorithms/bc_types.hrl
+++
@@ -1,5 +0,0 @@
--type broadcast() :: pid().
--type bc_message() :: beb_message()
-                 | rb_message().
--type beb_message() :: nonempty_string().
--type rb_message() :: {atom() | nonempty_string(), non_neg_integer(), beb_message()}.
